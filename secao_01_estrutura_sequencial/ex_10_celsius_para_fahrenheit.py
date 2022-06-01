19c425e839c8f7a34e9ec85cbead364999faf473
Sara Brangioni <sarabrangioni@gmail.com>
2022-06-01 15:02:01 -0300

    exercicio 10 resolvido

--- secao_01_estrutura_sequencial/ex_10_celsius_para_fahrenheit.py
+++ secao_01_estrutura_sequencial/ex_10_celsius_para_fahrenheit.py
@@ -18,3 +18,6 @@ Mostrar apenas valor inteiro da temperatura
 
 def transformar_para_fahrenheit():
     """Escreva aqui em baixo a sua solução"""
+    c= int(input("Digite a temperatura: "))
+    F= (c*1.8) + 32
+    print(f"Essa temperatura é de {round(F)} Fahrenheit")
