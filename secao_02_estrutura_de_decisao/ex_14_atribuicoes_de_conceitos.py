c4f8a095edc2c7d228ee68354b4d4db37f681897
Sara Brangioni <sarabrangioni@gmail.com>
2022-06-06 14:09:45 -0300

    exercicio 14

--- secao_02_estrutura_de_decisao/ex_14_atribuicoes_de_conceitos.py
+++ secao_02_estrutura_de_decisao/ex_14_atribuicoes_de_conceitos.py
@@ -46,3 +46,29 @@ Mostar valores com uma casa decimal.
 
 def calcular_conceito(nota_1: float, nota_2: float):
     """Escreva aqui em baixo a sua solução"""
+    media= (nota_1 + nota_2)/2
+    if media >= 9.0 and media <= 10.0:
+      print(f"Notas: {nota_1:.1f} e {nota_2:.1f}.")
+      print(f"Média: {media:.1f}")
+      print("Conceito: A")
+      print("Status: APROVADO")
+    if media >= 7.5 and media < 9.0:
+      print(f"Notas: {nota_1:.1f} e {nota_2:.1f}.")
+      print(f"Média: {media:.1f}")
+      print("Conceito: B")
+      print("Status: APROVADO")
+    if media >= 6.0 and media < 7.5:
+      print(f"Notas: {nota_1:.1f} e {nota_2:.1f}.")
+      print(f"Média: {media:.1f}")
+      print("Conceito: C")
+      print("Status: APROVADO")
+    if media >= 4.0 and media < 6.0:
+      print(f"Notas: {nota_1:.1f} e {nota_2:.1f}.")
+      print(f"Média: {media:.1f}")
+      print("Conceito: D")
+      print("Status: REPROVADO")
+    if media >= 0.0 and media < 4.0:
+      print(f"Notas: {nota_1:.1f} e {nota_2:.1f}.")
+      print(f"Média: {media:.1f}")
+      print("Conceito: E")
+      print("Status: REPROVADO")
